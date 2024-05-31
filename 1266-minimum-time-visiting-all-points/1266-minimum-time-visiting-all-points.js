365fb18b6f78fdb42aa37056620ba97cf19b7584
남홍근 <ghdrms1220@naver.com>
2024-05-31 10:43:17 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.js
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.js
@@ -4,10 +4,14 @@
  */
 var minTimeToVisitAllPoints = function (points) {
 
-   let result = 0;
-    for (let i = 0; i < points.length - 1; i++) {
-        result += Math.max(Math.abs(points[i][0] - points[i + 1][0]), Math.abs(points[i][1] - points[i + 1][1]));
+let result = 0;
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i < points.length; i++) {
+        x = Math.abs(points[i][0] - points[i + 1][0])
+        y = Math.abs(points[i][1] - points[i + 1][1])
+        result = Math.max(x,y)
     }
-    return result;
 
+    return result;
 };
